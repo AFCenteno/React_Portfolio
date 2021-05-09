8620b1f13ec66c071345dac6be0e78c5d7c77683
mrafcent1 <alexfcenteno@outlook.com>
2021-05-09 19:15:55 -0400

    edit about me text

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,9 +6,9 @@ const AboutMe = () => (
       <h1 className='col-12 pagetitle'>About Me</h1>
       <img className='myphoto col-sm-12 col-lg-6' src={img}></img>
       <p className='col-sm-12 col-lg-6 aboutcontent'>
-        Hey everyone, my name is Alex Centeno. Welcome to my portfolio! I am currently residing in 
+        Hey everyone, my name is Alex Centeno, welcome to my portfolio! I am currently residing in 
         Florida, working to become a full-time web developer. Feel free to browse my work and contact 
-        me using the contact page here.
+        me using the contact page available.
       </p>
     </div> 
 );

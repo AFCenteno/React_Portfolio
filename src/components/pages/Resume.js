dffa01a814af3b8e8736b0f9be5ef886a1cff656
mrafcent1 <alexfcenteno@outlook.com>
2021-05-09 19:30:43 -0400

    edit resume link to open in new tab

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,7 @@ import React from "react";
 const Resume = () => (
   <div>
     <h1 className='pagetitle'>My Resume</h1>
-    <a href='https://1drv.ms/w/s!Amu3nVVt7sMheNwxX_TJW-kdLBQ?e=nUNOkH'><strong>Download Resume</strong></a>
+    <a href='https://1drv.ms/w/s!Amu3nVVt7sMheNwxX_TJW-kdLBQ?e=nUNOkH' target="_blank"><strong>Download Resume</strong></a>
     <p className='resumecontent'>
       <strong>Front-end Proficiencies</strong>
       <li>HTML</li>

ed3a86a1b0647b13a958299304349cfdf9d81a1d
mrafcent1 <alexfcenteno@outlook.com>
2021-05-09 17:39:51 -0400

    contact form sending emails

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,27 @@
 import React from "react";
+import emailjs from 'emailjs-com'
 
-const onSubmit = data => console.log(data);
+export default function Contact() {
 
-const Contact = () => (
-  <div className='contactInfo'>
-    <form id='contact-form'>
-      <input type='text' name='user_name' placeholder='Name' />
-      <br/>
-      <input type='email' name='user_email' placeholder='Email' />
-      <br/>
-      <textarea name='message' placeholder='Message'/>
-      <br/>
-      <input type='submit' value='Send' />
-    </form>
-  </div>
-);
+  function sendEmail(e) {
+    e.preventDefault();
 
-export default Contact;
+    emailjs.sendForm('service_2e8a3qb', 'template_x5j9je1', '#form', 'user_kiDPxqFWUFpc3Q7kEcP6d')
+      .then((result) => {
+          console.log(result.text);
+          window.alert("Message has been sent!")
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form id='form' className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="to_name" value="Alex" />
+      <input type="text" name="from_name" placeholder="Enter Name" />
+      <input type="email" name="user_email" placeholder="Enter Email" />
+      <textarea name="message" placeholder="Enter Message" />
+      <input id="send" type="submit" value="Send" />
+    </form>  
+  );
+}

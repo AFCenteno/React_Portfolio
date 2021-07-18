2c3e49aaaffd2eeff540837a2c18dc46b99ace9c
mrafcent1 <alexfcenteno@outlook.com>
2021-07-18 03:16:06 -0400

    added bootcamp badge

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,5 +1,7 @@
 import React from "react";
 import img from './img/AC.jpg'
+import badge from './img/bootcamp.png'
+
 
 const AboutMe = () => (
     <div className='row'>
@@ -7,9 +9,11 @@ const AboutMe = () => (
         <img className=' col-sm-12 col-lg-12' src={img}></img>
       </div>
       <p className='col-sm-12 col-lg-12 aboutcontent'>
-        I'm Alex, a Full-stack web developer residing in sunny Florida. Check out my work and feel free to contact me about anything coding. 
+        I'm Alex, a full-stack web developer residing in sunny Florida. Check out my work and feel free to contact me about anything coding. 
       </p>
-    </div> 
+      <a className='badgelink col-2' href="https://badgr.com/public/assertions/cs_u8yAzTbKc02YeVYln_A" target="_blank"><img id="badge" src={badge}></img></a>
+    </div>
+     
 );
 
 export default AboutMe;

993cca78336d854ecc258d4e40a1ec48a0d78250
mrafcent1 <alexfcenteno@outlook.com>
2021-07-18 01:52:54 -0400

    updated more css this time for desktop

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,9 +4,9 @@ import img from './img/AC.jpg'
 const AboutMe = () => (
     <div className='row'>
       <div className='myphoto'>
-        <img className=' col-sm-12 col-lg-6' src={img}></img>
+        <img className=' col-sm-12 col-lg-12' src={img}></img>
       </div>
-      <p className='col-sm-12 col-lg-6 aboutcontent'>
+      <p className='col-sm-12 col-lg-12 aboutcontent'>
         I'm Alex, a Full-stack web developer residing in sunny Florida. Check out my work and feel free to contact me about anything coding. 
       </p>
     </div> 

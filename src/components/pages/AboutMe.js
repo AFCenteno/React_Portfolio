4115311c401149583a3613434f860fb2dbdec706
mrafcent1 <alexfcenteno@outlook.com>
2021-07-19 15:52:51 -0400

    updated css for desktop

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => (
         <img className=' col-sm-12 col-lg-12' src={img}></img>
       </div>
       <p className='col-sm-12 col-lg-12 aboutcontent'>
-        I'm Alex, a full-stack web developer residing in sunny Florida. Check out my work and feel free to contact me about anything coding. 
+        I'm Alex, a full-stack web developer residing in sunny Florida. Check out my work and feel free to contact me about anything coding related. 
       </p>
       <a className='badgelink col-2' href="https://badgr.com/public/assertions/cs_u8yAzTbKc02YeVYln_A" target="_blank"><img id="badge" src={badge}></img></a>
     </div>
